651fafcc2bb0d4194690c85c9e63966ddb2eb76c
vetzals <144376232+vetzals@users.noreply.github.com>
2023-11-25 17:43:59 -0500

    Update GameMechs.cpp

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -1,53 +1,107 @@
 #include "GameMechs.h"
+#include "MacUILib.h"
 
 GameMechs::GameMechs()
 {
+    input = 0;
+    exitFlag=false;
+    loseFlag=false;
+    boardSizeX=30;
+    boardSizeY=15;
+    score=0;
+
 
 }
 
 GameMechs::GameMechs(int boardX, int boardY)
 {
+    input = 0;
+    exitFlag=false;
+    loseFlag=false;
+    boardSizeX=boardX;
+    boardSizeY=boardY;
+    score=0;
+
 
 }
 
 // do you need a destructor?
+GameMechs::~GameMechs()
+{
 
+}
 
 
 bool GameMechs::getExitFlagStatus()
 {
+    return exitFlag;
+
+
+}
+
+void GameMechs::setExitTrue()
+{
+    exitFlag=true;
+
+}
+
+bool GameMechs::getLoseFlagStatus()
+{
+    return loseFlag;
+
+}
+
+void GameMechs::setLoseFlag()
+{
+    loseFlag=true;
 
 }
 
 char GameMechs::getInput()
 {
-
+    if (MacUILib_hasChar())
+    { 
+        input = MacUILib_getChar();
+    }
 }
+
 
 int GameMechs::getBoardSizeX()
 {
+    return boardSizeX;
 
 }
 
 int GameMechs::getBoardSizeY()
 {
+    return boardSizeY;
 
 }
 
-
-void GameMechs::setExitTrue()
-{
-
-}
 
 void GameMechs::setInput(char this_input)
 {
+    input = this_input;
 
 }
 
 void GameMechs::clearInput()
 {
+    input=0;
 
 }
+
+int GameMechs::getScore()
+{
+    return score;
+
+}
+
+void GameMechs::incrementScore()
+{
+    score++;
+
+}
+
 
 
