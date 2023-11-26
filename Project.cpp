e5bac39545ec609900e88ef984781e1ac86986d7
sarfrazf31 <144144940+sarfrazf31@users.noreply.github.com>
2023-11-25 19:45:29 -0500

    Update Project.cpp
    iteration 1 draft 1

--- Project.cpp
+++ Project.cpp
@@ -9,6 +9,10 @@ using namespace std;
 
 bool exitFlag;
 
+GameMechs* myGM;
+Player* myPlayer;
+
+
 void Initialize(void);
 void GetInput(void);
 void RunLogic(void);
@@ -42,21 +46,49 @@ void Initialize(void)
     MacUILib_clearScreen();
 
     exitFlag = false;
+
+    myGM = new GameMechs(30, 15);
+    myPlayer = new Player(myGM);
 }
 
 void GetInput(void)
 {
-   
+
 }
 
 void RunLogic(void)
 {
-    
+    myPlayer->updatePlayerDir();
+    myPlayer->movePlayer();
 }
 
 void DrawScreen(void)
 {
-    MacUILib_clearScreen();    
+    MacUILib_clearScreen();
+    objPos tempPos;
+    myPlayer->getPlayerPos(tempPos); //get player position
+    
+    for(int j = 0, j < 15, j++)
+    {
+        for(int i = 0, i < 30, i++)
+        {
+            if (i == 0 || i == 29 || j == 0 || j == 14)
+            {
+                MacUILib_printf("#");
+            }
+            else if (i == tempPos.x && j == tempPos.y)
+            {
+                MacUILib_printf("%c", tempPos.symbol);
+            }
+            else
+            {
+                MacUILib_printf(" ");
+            }
+        }
+        MacUILib_printf("\n");
+    }  
+
+    MacUILib_printf("Board size: %d%d, Player position: <%d, %d> +%c\n", myGM->getBoardSizeX(), myGM->getBoardSizeY(), tempPos.x, tempPos.y, tempPos.symbol);   
 
 }
 
