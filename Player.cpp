e70773b2a7c793054a174cf9b93268a9fe23ad88
sarfrazf31 <144144940+sarfrazf31@users.noreply.github.com>
2023-11-25 19:42:27 -0500

    Update Player.cpp
    iteration 1 draft 1

--- Player.cpp
+++ Player.cpp
@@ -1,12 +1,12 @@
 #include "Player.h"
+#include "objPos.h"
 
 
 Player::Player(GameMechs* thisGMRef)
 {
     mainGameMechsRef = thisGMRef;
     myDir = STOP;
-
-    // more actions to be included
+    playerPos.setObjPos(mainGameMechsRef->getBoardSizeX(), mainGameMechsRef->getBoardSizeY(), '*');
 }
 
 
@@ -18,15 +18,92 @@ Player::~Player()
 void Player::getPlayerPos(objPos &returnPos)
 {
     // return the reference to the playerPos arrray list
+    returnPos.setObjPos(playerPos.x, playerPos.y, playerPos.symbol);
 }
 
 void Player::updatePlayerDir()
 {
-    // PPA3 input processing logic        
+    // PPA3 input processing logic 
+    if (mainGameMechsRef->GetInput()) //does saying this makes sense? im trying to say that there is input presence
+    {
+        switch(mainGameMechsRef->GetInput())
+        {                      
+            case '^':  // exit
+                mainGameMechsRef->setExitTrue();
+                break;
+            case 'w':
+                if (myDir!=DOWN)
+                {
+                    myDir = UP;
+                }
+                break;
+            case 's':
+                if (mydir!=UP)
+                {
+                    myDir = DOWN;
+                }
+                break;
+            case 'a':
+                if (myDir!=RIGHT)
+                {
+                    myDir = LEFT;
+                }
+                break;
+            case 'd':
+                if (myDir!=LEFT)
+                {
+                    myDir = RIGHT;
+                }
+                break;
+            default:
+                break;
+        }       
+    }
 }
 
 void Player::movePlayer()
 {
     // PPA3 Finite State Machine logic
+    int moveCount++;
+
+    //Update player location by one unit
+    if (myDir==UP)
+    {
+         playerPos.y--;
+         moveCount++;
+    }
+    else if (myDir==DOWN)
+    {
+        playerPos.y++;
+        moveCount++;
+    }
+    else if (myDir==LEFT)
+    {
+        playerPos.x--;
+        moveCount++;
+    }
+    else if (myDir==RIGHT)
+    {
+        playerPos.x++;
+        moveCount++;
+    }
+
+    //Border wraparound 
+    if (playerPos.x<0)
+    {
+        playerPos.x = mainGameMechsRef->getBoardSizeX-1;
+    }
+    else if (playerPos.x >= mainGameMechsRef->getBoardSizeX)
+    {
+        playerPos.x = 0;
+    }
+    else if (playerPos.y<0)
+    {
+        playerPos.y = mainGameMechsRef->getBoardSizeY-1;
+    }
+    else if (playerPos.y>=mainGameMechsRef->getBoardSizeY)
+    {
+        playerPos.y = 0;
+    }
 }
 
